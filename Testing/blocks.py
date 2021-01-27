a372b9944bb4c2724993e87a93bec39e5299dcda
Keywarn <charliehasalamb@icloud.com>
2021-01-27 18:13:53 +0000

    Basic set block and get block working

--- Testing/blocks.py
+++ Testing/blocks.py
@@ -3,11 +3,9 @@ import requests
 url = "http://localhost:9000"
 
 def SetBlock(x,y,z, block):
-    print(f"Setting X: {x}, Y: {y}, Z: {z} - block")
-    r = requests.put(url+f"/blocks?x={x}&y={y}&z={z}", data = "block")
-    print(r)
+    print(f"Setting X: {x}, Y: {y}, Z: {z} - {block}")
+    r = requests.put(url+f"/blocks?x={x}&y={y}&z={z}", data = block)
 
 def GetBlock(x,y,z):
-    print(f"Setting X: {x}, Y: {y}, Z: {z} - block")
     r = requests.get(url+f"/blocks?x={x}&y={y}&z={z}")
-    print(r)
+    print(f"Block X: {x}, Y: {y}, Z: {z} - {r.content}")
