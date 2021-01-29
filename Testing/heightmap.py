777efc7af4415592cdbf8ce2b4fb87ace141851e
Keywarn <charliehasalamb@icloud.com>
2021-01-29 17:27:45 +0000

    convert to chunks earlier

--- Testing/heightmap.py
+++ Testing/heightmap.py
@@ -5,25 +5,28 @@ from io import BytesIO
 
 url = "http://localhost:9000"
 
-#Take in two locations (as tuples) and get the chunks containing the area
+#Take in two chunk locations and get the chunks containing the area
 def GetChunks(a, b, rtype = 'text'):
-    print(f"Getting chunks in area A: {a}, B: {b}")
-
-    #Start with the bottom left chunk
-    chunkA = (min(a[0],b[0])//16, min(a[1],b[1])//16)
-    chunkB = (max(a[0],b[0])//16, max(a[1],b[1])//16)
-
     acceptType = 'application/octet-stream' if rtype == 'bytes' else 'text/raw'
-    r = requests.get(url+f"/chunks?x={chunkA[0]}&z={chunkA[1]}&dx={chunkB[0]-chunkA[0]}&dz={chunkB[1]-chunkA[1]}", headers={"Accept": acceptType})
+    r = requests.get(url+f"/chunks?x={a[0]}&z={a[1]}&dx={b[0]-a[0]}&dz={b[1]-a[1]}", headers={"Accept": acceptType})
     
     if rtype == 'text':
         return r.text
     elif rtype == 'bytes':
         return r.content
 
+def CoordToChunks(a,b):
+    #Return the chunk locations
+    chunkA = (min(a[0],b[0])//16, min(a[1],b[1])//16)
+    chunkB = (max(a[0],b[0])//16, max(a[1],b[1])//16)
+
+    return chunkA, chunkB
+
 #Take in two locations (as tuples) and get the heightmap
 def GetHeightmap(a,b):
-    data = GetChunks(a,b, rtype='bytes')
+
+    chunkA, chunkB = CoordToChunks(a,b)
+    data = GetChunks(chunkA,chunkB, rtype='bytes')
     file = BytesIO(data)
     
     nbtFile = nbt.nbt.NBTFile(buffer=file)
