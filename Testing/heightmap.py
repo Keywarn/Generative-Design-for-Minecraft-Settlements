f31e66c79ba6397c89d0c5c52a3af8bb2b841038
Keywarn <charliehasalamb@icloud.com>
2021-01-29 16:43:24 +0000

    Remove to get chunks section

--- Testing/heightmap.py
+++ Testing/heightmap.py
@@ -2,12 +2,21 @@ import requests
 
 url = "http://localhost:9000"
 
-#Take in two locations (as tuples) and get the heightmaps for the chunks containing the area
-def GetHeightmap(a, b):
+#Take in two locations (as tuples) and get the chunks containing the area
+def GetChunks(a, b, rtype = 'text'):
     print(f"Getting chunks in area A: {a}, B: {b}")
 
     #Start with the bottom left chunk
     chunkA = (min(a[0],b[0])//16, min(a[1],b[1])//16)
     chunkB = (max(a[0],b[0])//16, max(a[1],b[1])//16)
-    r = requests.get(url+f"/chunks?x={chunkA[0]}&z={chunkA[1]}&dx={chunkB[0]-chunkA[0]}&dz={chunkB[1]-chunkA[1]}")
-    print(r.content)
+
+    acceptType = 'application/octet-stream' if rtype == 'bytes' else 'text/raw'
+    r = requests.get(url+f"/chunks?x={chunkA[0]}&z={chunkA[1]}&dx={chunkB[0]-chunkA[0]}&dz={chunkB[1]-chunkA[1]}", headers={"Accept": acceptType})
+    
+    if rtype == 'text':
+        return response.text
+    elif rtype == 'bytes':
+        return response.content
+
+#Take in two locations (as tuples) and get the heightmap
+def GetHeightmap(a,b):
