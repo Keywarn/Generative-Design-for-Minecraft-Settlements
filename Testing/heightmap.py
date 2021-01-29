f5d8d221935bb8f65d80a4df8195414cd5d5e0bf
Keywarn <charliehasalamb@icloud.com>
2021-01-29 18:33:24 +0000

    reading from surface map

--- Testing/heightmap.py
+++ Testing/heightmap.py
@@ -2,13 +2,14 @@ import requests
 import nbt
 
 from io import BytesIO
+from bitarray import BitArray
 
 url = "http://localhost:9000"
 
 #Take in two chunk locations and get the chunks containing the area
 def GetChunks(a, b, rtype = "text"):
     acceptType = "application/octet-stream" if rtype == "bytes" else "text/raw"
-    r = requests.get(url+f"/chunks?x={a[0]}&z={a[1]}&dx={b[0]-a[0]}&dz={b[1]-a[1]}", headers={"Accept": acceptType})
+    r = requests.get(url+f"/chunks?x={a[0]}&z={a[1]}&dx={b[0]-a[0]+1}&dz={b[1]-a[1]+1}", headers={"Accept": acceptType})
     
     if rtype == "text":
         return r.text
@@ -22,9 +23,15 @@ def CoordToChunks(a,b):
 
     return chunkA, chunkB
 
+def OrderCoords(a,b):
+    aa = (min(a[0],b[0]), min(a[1],b[1]))
+    bb = (max(a[0],b[0]), max(a[1],b[1]))
+
+    return aa,bb
+
 #Take in two locations (as tuples) and get the heightmap
 def GetHeightmap(a,b):
-
+    a,b = OrderCoords(a,b)
     chunkA, chunkB = CoordToChunks(a,b)
     data = GetChunks(chunkA,chunkB, rtype="bytes")
     file = BytesIO(data)
@@ -32,14 +39,21 @@ def GetHeightmap(a,b):
     nbtFile = nbt.nbt.NBTFile(buffer=file)
 
     #Create empty map:
-    heightmap = [[-1 for z in range(abs(b[1]-a[1]))] for x in range(abs(b[0]-a[0]))]
+    heightmap = [[-1 for z in range(b[1]-a[1])] for x in range(b[0]-a[0])]
 
     #For each chunk go through co-ords
-    for x in range(chunkB[0]-chunkA[0]):
-        for z in range(chunkB[1]-chunkA[1]):
+    for x in range(chunkB[0]-chunkA[0]+1):
+        for z in range(chunkB[1]-chunkA[1]+1):
 
             chunkNum = x + z * (chunkB[1]-chunkA[1])
 
-            nbtFile["Chunks"][chunkNum]["Level"]["Heightmaps"]["WORLD_SURFACE"]
+            rawMap = nbtFile["Chunks"][chunkNum]["Level"]["Heightmaps"]["WORLD_SURFACE"]
+            mapBitArray = BitArray(9, 16*16, rawMap)
+
+            #Index other way round in NBT
+            for cz in range(16):
+                for cx in range(16):
+
+                    heightmap[x * 16 + cx][z * 16 + cz] = mapBitArray.getAt(cz * 16 + cx)
 
 GetHeightmap((0,0), (100,100))
