0a544f432c786deb7b88b43e72beaf7502f413ec
Keywarn <charliehasalamb@icloud.com>
2021-01-29 17:47:23 +0000

    cycle through chunks

--- Testing/heightmap.py
+++ Testing/heightmap.py
@@ -6,13 +6,13 @@ from io import BytesIO
 url = "http://localhost:9000"
 
 #Take in two chunk locations and get the chunks containing the area
-def GetChunks(a, b, rtype = 'text'):
-    acceptType = 'application/octet-stream' if rtype == 'bytes' else 'text/raw'
+def GetChunks(a, b, rtype = "text"):
+    acceptType = "application/octet-stream" if rtype == "bytes" else "text/raw"
     r = requests.get(url+f"/chunks?x={a[0]}&z={a[1]}&dx={b[0]-a[0]}&dz={b[1]-a[1]}", headers={"Accept": acceptType})
     
-    if rtype == 'text':
+    if rtype == "text":
         return r.text
-    elif rtype == 'bytes':
+    elif rtype == "bytes":
         return r.content
 
 def CoordToChunks(a,b):
@@ -26,11 +26,20 @@ def CoordToChunks(a,b):
 def GetHeightmap(a,b):
 
     chunkA, chunkB = CoordToChunks(a,b)
-    data = GetChunks(chunkA,chunkB, rtype='bytes')
+    data = GetChunks(chunkA,chunkB, rtype="bytes")
     file = BytesIO(data)
     
     nbtFile = nbt.nbt.NBTFile(buffer=file)
 
-    print(nbtFile)
+    #Create empty map:
+    heightmap = [[-1 for z in range(abs(b[1]-a[1]))] for x in range(abs(b[0]-a[0]))]
+
+    #For each chunk go through co-ords
+    for x in range(chunkB[0]-chunkA[0]):
+        for z in range(chunkB[1]-chunkA[1]):
+
+            chunkNum = x + z * (chunkB[1]-chunkA[1])
+
+            nbtFile["Chunks"][chunkNum]["Level"]["Heightmaps"]["WORLD_SURFACE"]
 
 GetHeightmap((0,0), (100,100))
