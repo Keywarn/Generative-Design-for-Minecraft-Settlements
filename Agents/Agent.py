d83e2087a0985ae225f12f9370b8d04a5fe3e3e1
Keywarn <charliehasalamb@icloud.com>
2021-02-11 18:28:12 +0000

    Basic Pathfinder

--- Agents/Agent.py
+++ Agents/Agent.py
@@ -40,9 +40,77 @@ class PathFinder:
     def __init__(self, hm):
         self.heightmap = hm
     
-    def findPath(a,b,swim=False, fall=False):
-        print("Finding path")
+    def distance(self,a,b):
+        return max(abs(a[0] - b[0]),abs(a[1] - b[1]))
+    
+    def findPath(self, a,b,swim=False, fall=False):
+        neighbours = [[0,1],[1,1],[1, 0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
 
+        maze = [[Cell(x, z) for z in range(len(self.heightmap[0]))] for x in range(len(self.heightmap))]
+        maze[a[0]][a[1]].open = True
+        openList =[maze[a[0]][a[1]]]
+
+        while len(openList) != 0:
+            cur = openList[0]
+            #Get cell with lowest f cost
+            index = 0;
+            for i in range(len(openList)):
+                if(openList[i].fCost < cur.fCost):
+                    cur = openList[i]
+                    index = i
+            
+            openList.remove(cur)
+            cur.open = False
+            cur.closed = True
+
+            #Check current isn't the final cell
+            if(cur.x == b[0] and cur.z == b[1]):
+                #TODO RETURN THE PATH
+                break
+            else:
+                #For each neighbour
+                for i in range(8):
+                    x = cur.x + neighbours[i][0]
+                    z = cur.z + neighbours[i][1]
+
+                    #Check neighbour is on board
+                    if((x >= 0 and x < len(maze)) and (z >= 0 and z < len(maze[0]))):
+                        #Check if can travel to the block
+                        #TODO Checks for water and falling
+                        if(abs(self.heightmap[cur.x][cur.z]-self.heightmap[x][z]) < 2):
+
+                            #Calculate new fCost
+                            gNew = cur.gCost + 1
+                            hNew = self.distance([x,z],b)
+                            fNew = gNew + hNew
+                            if(not maze[x][z].open or fNew < maze[x][z].fCost):
+                                #update
+                                maze[x][z].fCost = fNew
+                                maze[x][z].gCost = gNew
+                                maze[x][z].hCost = hNew
+                                maze[x][z].parent = [cur.x, cur.z]
+                                #add to open list if needed
+                                if(not maze[x][z].open):
+                                    maze[x][z].open = True
+                                    openList.append(maze[x][z])
+        
+        print("FINISHED PATH")
+class Cell:
+    def __init__(self, x, z):
+        #Distance from a
+        self.gCost = 0
+        #Distance to b
+        self.hCost = 0
+        #Combined
+        self.fCost = 999999
+
+        self.open = False
+        self.closed = False
+
+        self.x = x
+        self.z = z
+
+        self.parent = []
 
 
 
--- Generator.py
+++ Generator.py
@@ -7,6 +7,8 @@ heightmap = mapTools.GetHeightmap(a, b)
 ag = agent.Agent([6,71,146], b'minecraft:obsidian')
 pf = agent.PathFinder(heightmap)
 
+pf.findPath([0,0],[10,10])
+
 for i in range(10):
     ag.move([6,71, 146+i])
 
