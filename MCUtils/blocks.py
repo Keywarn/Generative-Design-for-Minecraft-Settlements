dace17b1e4c5be2c312722e9b1a93cff253abbd5
Keywarn <charliehasalamb@icloud.com>
2021-02-11 16:15:03 +0000

    Changed testing to MCUtils

--- MCUtils/blocks.py
+++ MCUtils/blocks.py
@@ -3,7 +3,7 @@ import requests
 url = "http://localhost:9000"
 
 def SetBlock(x,y,z, block):
-    print(f"Setting X: {x}, Y: {y}, Z: {z} - {block}")
+    #print(f"Setting X: {x}, Y: {y}, Z: {z} - {block}")
     r = requests.put(url+f"/blocks?x={x}&y={y}&z={z}", data = block)
 
 def GetBlock(x,y,z):
