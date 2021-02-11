580a13dbc01db8fd726333391768dab60a76f441
Keywarn <charliehasalamb@icloud.com>
2021-02-11 16:18:55 +0000

    import stuff

--- Agents/Agent.py
+++ Agents/Agent.py
@@ -1,12 +1,11 @@
 
-
-class Agent(:
-    _def __init__(self, pos, block):
+class Agent:
+    def __init__(self, pos, block):
 
         self.pos = pos
         self.block = block
 
-        self.prevBlock = GetBlock(pos[0], pos[1])
+        #self.prevBlock = GetBlock(pos[0], pos[1])
 
         self.move(pos)
 
@@ -14,9 +13,9 @@ class Agent(:
         #Should check if it is a valid move first
 
         #Set the block of current position back
-        SetBlock(self.pos, prevBlock)
+        #SetBlock(self.pos, prevBlock)
         #Move to new position
         self.pos = pos
         #Set the block
-        SetBlock(pos, block)
+        #SetBlock(pos, block)
 
---
+++ Generator.py
@@ -0,0 +1,4 @@
+from Agents import Agent
+
+
+print("done")
