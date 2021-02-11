bd4f34ee6ff60ce7f05c624b241391f48c58a24b
Keywarn <charliehasalamb@icloud.com>
2021-02-11 16:13:26 +0000

    Basic Agent Layout

---
+++ Agents/Agent.py
@@ -0,0 +1,22 @@
+
+
+class Agent(:
+    _def __init__(self, pos, block):
+
+        self.pos = pos
+        self.block = block
+
+        self.prevBlock = GetBlock(pos[0], pos[1])
+
+        self.move(pos)
+
+    def move(pos):
+        #Should check if it is a valid move first
+
+        #Set the block of current position back
+        SetBlock(self.pos, prevBlock)
+        #Move to new position
+        self.pos = pos
+        #Set the block
+        SetBlock(pos, block)
+
