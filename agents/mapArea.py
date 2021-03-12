8c16fdbd3b636ea2080a90becaa6ee9af009b0a7
Keywarn <charliehasalamb@icloud.com>
2021-03-12 10:37:19 +0000

    Tree class in map area

--- agents/mapArea.py
+++ agents/mapArea.py
@@ -6,3 +6,11 @@ class MapArea:
         self.size = (b[0]-a[0] +1,b[1]-a[1] +1)
         self.heightmap = [[-1 for z in range(self.size[1])] for x in range(self.size[0])]
         self.blockMap = [[None for z in range(self.size[1])] for x in range(self.size[0])]
+
+        self.trees = []
+
+class Tree:
+
+    def __init__(self, pos, type):
+        self.pos = pos
+        self.type = type
