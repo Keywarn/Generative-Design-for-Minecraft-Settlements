04c73a6646d4aa43dc32fd8f8b166da1aa7d2eaa
Keywarn <charliehasalamb@icloud.com>
2021-04-20 16:24:14 +0100

    Fixed flattening

--- agents/agent.py
+++ agents/agent.py
@@ -333,16 +333,22 @@ class Builder:
         #First clear the plot and even out ground
         for x in range (a[0], b[0]):
             for z in range (a[1], b[1]):
-                for height in range (gHeight, self.world.heightmap[x][z] -1, -1 if self.world.heightmap[x][z] <= gHeight else 1):
-
-                    if(height <= gHeight):
+                worldHeight =  self.world.heightmap[x][z] -1
+            
+                #needs raising
+                if(worldHeight < gHeight):
+                    for height in range(worldHeight, gHeight):
                         blocks.SetBlock([x+self.world.a[0], height, z+self.world.a[1]], ground)
-                        if(height < gHeight):
-                            self.world.heightmap[x][z] += 1
 
-                    if(height > gHeight):
+                #Needs lowering
+                else:
+                    for height in range (worldHeight, gHeight, -1):
                         blocks.SetBlock([x+self.world.a[0], height, z+self.world.a[1]], b'minecraft:air')
                         self.world.heightmap[x][z] -= 1
+
+                #Ensure ground is correct
+                blocks.SetBlock([x+self.world.a[0], gHeight, z+self.world.a[1]], ground)
+                self.world.heightmap[x][z] = gHeight + 1
                     
                 self.world.blockMap[x][z] = ground
 
